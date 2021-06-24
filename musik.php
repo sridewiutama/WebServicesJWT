f69fbbbe2df0ac7a1f7fe34b42d45f34567f6f29
sridewiutama <83205901+sridewiutama@users.noreply.github.com>
2021-06-24 18:54:23 +0700

    Add files via upload

---
+++ musik.php
@@ -0,0 +1,55 @@
+<?php
+// Import script autoload agar bisa menggunakan library
+require_once('./vendor/autoload.php');
+// Import library
+use Firebase\JWT\JWT;
+use Dotenv\Dotenv;
+
+// Load dotenv
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Atur jenis response
+header('Content-Type: application/json');
+
+// Cek method request
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+  http_response_code(405);
+  exit();
+}
+
+$headers = getallheaders();
+
+// Periksa apakah header authorization-nya ada
+if (!isset($headers['Authorization'])) {
+  http_response_code(401);
+  exit();
+}
+
+// Mengambil token
+list(, $token) = explode(' ', $headers['Authorization']);
+
+try {
+  // Men-decode token. Dalam library ini juga sudah sekaligus memverfikasinya
+  JWT::decode($token, $_ENV['ACCESS_TOKEN_SECRET'], ['HS256']);
+  
+  $musik = [
+    [
+      'judul' => 'Separuh aku',
+      'penyayi' => 'Ariel Noah',
+      'genre' => 'POP'
+    ],
+    [
+      'Judul' => 'Kota',
+      'penyayi' => 'Dere',
+      'genre' => 'POP'
+    ]
+  ];
+
+
+  echo json_encode($musik);
+} catch (Exception $e) {
+  // Bagian ini akan jalan jika terdapat error saat JWT diverifikasi atau di-decode
+  http_response_code(401);
+  exit();
+}
